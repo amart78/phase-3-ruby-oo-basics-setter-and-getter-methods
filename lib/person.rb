f83d42e1df4ffa0a878140b70ce493df1b3aa446
Alexis <amart78@outlook.com>
2022-04-09 17:34:54 -0500

    commit tree 52a46b804aafb9cb50a3c9121f6414835b31ca48

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
+# Define a name getter and a name= setter method for the instance variable @name.
+# Define a job getter and a job= setter method for the instance variable @job.
+
 class Person
+    def name=(name)
+    @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def job=(job)
+    @job = job
+    end
+
+    def job 
+        @job
+    end
 
 end
